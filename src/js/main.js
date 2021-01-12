0defcbf8404242c9f2ca57e15747d6c541000389
Ed <edmundoj.presas@gmail.com>
2021-01-12 21:21:47 +0100

    feat(*): WIP - initial webpack setup

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+{/* <script src="js/vendor/jquery.min.js"></script>
+<script src="js/vendor/popper.min.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script src="./js/vendor/owl.carousel.min.js"></script>
+<script src="./js/script.js"></script> */}
+
+import $ from 'jquery';
+
+$(document).ready(() => {
+  alert('Hola');
+});
